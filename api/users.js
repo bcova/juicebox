0c2b3f5e505c9116b5e940282024f8d98601a8e4
bcova <brandoncova@gmail.com>
2022-07-19 12:55:27 -0500

    Routes

---
+++ api/users.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const usersRouter = express.Router();
+
+usersRouter.use((req, res, next) => {
+  console.log("A request is being made to /users");
+
+  next(); 
+});
+
+usersRouter.get('/', (req, res) => {
+  res.send({
+    users: []
+  });
+});
+
+module.exports = usersRouter;
+
